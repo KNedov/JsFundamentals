546b1be1d5927802323ed1c83423de79ca599def
KNedov <k.medov.90@gmail.com>
2023-11-22 12:22:29 +0200

    Upload

--- Arrays-More exercises/nonDecreasingSubset.js
+++ Arrays-More exercises/nonDecreasingSubset.js
@@ -1,18 +1,14 @@
 function nonDecreasingSubset(array) {
-    let newArray=[];
-for (let i = 0; i < array.length; i++) {
- let element1 = array[i];
- for (let j = i+1; j < array.length; j++) {
-    let element2 = array[j];
-    if (element1<element2){
-newArray.push(element1)
+  let newArray = [];
+  let element1 = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (element1 <= array[i]) {
+      newArray.push(array[i]);
+      element1 = array[i];
     }
-    
- }
-    
-}console.log(newArray);
-    
+  }
+  console.log(newArray.join(' '));
 }
-nonDecreasingSubset([ 1, 3, 8, 4, 10, 12, 3, 2, 24])
-nonDecreasingSubset([ 1, 2, 3, 4])
-nonDecreasingSubset([ 20, 3, 2, 15, 6, 1])
+nonDecreasingSubset([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+nonDecreasingSubset([1, 2, 3, 4]);
+nonDecreasingSubset([20, 3, 2, 15, 6, 1]);
